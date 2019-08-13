865f354badc9651328920ec7397173d566da5a06
abhidahiya23 <abhidahiya23@gmail.com>
2019-08-13 12:53:33 +0530

    code

--- CodeChef/CHEFDIL_Dilemma.cpp
+++ CodeChef/CHEFDIL_Dilemma.cpp
@@ -1,14 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int swap(){}
+
+void showq(queue <int> gq)
+{
+    queue <int> g = gq;
+    while (!g.empty())
+    {
+        cout << '\t' << g.front();
+        g.pop();
+    }
+    cout << '\n';
+}
+
 int main(){
 
     int t;
     cin>>t;
     // t is no of test cases
     while(t--){
-        int n;
-        cin>>n;
+        string str;
+        cin>>str;
+
+        queue <int> circle;
+        int i=0,temp;
+        while(str[i] != '\0'){
+            temp = str[i];
+            circle.push(temp);
+            i++;
+        }
+        circle.push(10);
+        cout << "The queue gquiz is : ";
+        showq(circle);
+
 
     }
 
